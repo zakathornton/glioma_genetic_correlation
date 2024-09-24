1995a2e7c2d7bfdd70b7280047227b630f5938bb
Zak Thornton <86413667+zakathornton@users.noreply.github.com>
2024-09-24 15:01:24 +0100

    Create PWCoCo.sh

---
+++ PWCoCo/PWCoCo.sh
@@ -0,0 +1,10 @@
+./pwcoco/build/pwcoco \
+         --bfile GRCh37_EUR \
+         --chr 1 \
+         --maf 0.01 \
+         --init_h4 100 \
+         --out_cond \
+         --sum_stats1 prostate_pwcoco_157.txt \
+         --sum_stats2 all_glioma_pwcoco_157.txt \
+         --out prostate_all_pwcoco_157 \
+         /
