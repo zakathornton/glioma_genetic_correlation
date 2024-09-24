22a57463b8a40d5039dfc6ae133417e23250d683
Zak Thornton <86413667+zakathornton@users.noreply.github.com>
2024-09-24 15:13:49 +0100

    Update munge_sumstats.sh

--- LDSC/munge_sumstats.sh
+++ LDSC/munge_sumstats.sh
@@ -1,6 +1,6 @@
-module load languages/python/ldsc-2.0.1
-munge_sumstats.py \
---sumstats oropharyngeal_data.txt \
---out oropharyngeal_rashkin \
---merge-alleles w_hm3.snplist \
---chunksize 500000
+module load languages/python/ldsc-2.0.1      #load module
+munge_sumstats.py \                          #run the python script
+--sumstats oropharyngeal_data.txt \          #select the summary stats to be munged
+--out oropharyngeal_rashkin \                #define the outcome name
+--merge-alleles w_hm3.snplist \              #provide the SNP list
+--chunksize 500000                           #define the chunksize i.e., the number of SNPs to be ran at a time.
