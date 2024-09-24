16b11ab21d597986b5e6b5aabdc6bb393dac2daa
Zak Thornton <86413667+zakathornton@users.noreply.github.com>
2024-09-24 15:09:16 +0100

    Create heritability.sh

---
+++ LDSC/heritability.sh
@@ -0,0 +1,6 @@
+module load languages/python/ldsc-2.0.1
+ldsc.py \
+--h2 oropharyngeal_rashkin.sumstats.gz \
+--ref-ld-chr eur_w_ld_chr/ \
+--w-ld-chr eur_w_ld_chr/ \
+--out oropharyngeal_rashkin_h2
