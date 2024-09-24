f3cbf07999993a1f9090aa3143b5ba67811f09e0
Zak Thornton <86413667+zakathornton@users.noreply.github.com>
2024-09-24 15:42:19 +0100

    Create BCC_cleaning.R

---
+++ Data_cleaning/BCC_cleaning.R
@@ -0,0 +1,47 @@
+## Load modules
+
+library(data.table)
+
+## Load the raw data
+
+data<-read.delim("bcc_raw.tsv")
+
+## Extract the variables needed (CHR, POS, SNP, A1, A2, FREQA1, BETA, SE, P, N)
+
+data<-data[c(1,3,2,4,5,6,7,8,9,10)]
+
+## Appropriately rename columns
+
+colnames(data)[1]<-"CHR"
+colnames(data)[2]<-"POS"
+colnames(data)[3]<-"SNP"
+colnames(data)[4]<-"A1"
+colnames(data)[5]<-"A2"
+colnames(data)[6]<-"FREQA1"
+colnames(data)[7]<-"BETA"
+colnames(data)[8]<-"SE"
+colnames(data)[9]<-"P"
+colnames(data)[10]<-"N"
+
+## Ensure all numeric values are numeric
+
+data$FREQA1<-as.numeric(data$FREQA1)
+data$BETA<-as.numeric(data$BETA)
+data$SE<-as.numeric(data$SE)
+data$P<-as.numeric(data$P)
+
+## Only keep autosomal chromosomes
+
+data<-data[data$CHR%between%c(1,22),]
+
+## Remove any SNPs with MAF<0.01
+
+data<-data[data$FREQA1>0.01&data$FREQA1<0.99,]
+
+## Keep unique SNPs only (remove duplicates)
+
+data<-data[!duplicated(data$SNP),]
+
+## Save the new data
+
+write.table(data, "bcc_data.txt", quote = F, row.names = F)
