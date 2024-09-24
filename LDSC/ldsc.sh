98633016a71337516685b76f1beb013107e09f39
Zak Thornton <86413667+zakathornton@users.noreply.github.com>
2024-09-24 15:26:31 +0100

    Update ldsc.sh

--- LDSC/ldsc.sh
+++ LDSC/ldsc.sh
@@ -1,6 +1,6 @@
-module load languages/python/ldsc-2.0.1
-ldsc.py \
---rg oropharyngeal_rashkin.sumstats.gz, all_glioma.sumstats.gz, gbm_glioma.sumstats.gz, nongbm_glioma.sumstats.gz \
---ref-ld-chr eur_w_ld_chr/ \
---w-ld-chr eur_w_ld_chr/ \
---out oropharyngeal_rashkin_gc
+module load languages/python/ldsc-2.0.1                      #load modules
+ldsc.py \                                                    #run the python script
+--rg oropharyngeal_rashkin.sumstats.gz, all_glioma.sumstats.gz, gbm_glioma.sumstats.gz, nongbm_glioma.sumstats.gz \            #provide the zipped summary stats for genetic correlation
+--ref-ld-chr eur_w_ld_chr/ \                                 # provide the LD scores for LDSC
+--w-ld-chr eur_w_ld_chr/ \                                   # provide the LD scores for regression weights
+--out oropharyngeal_rashkin_gc                               #provide the 
