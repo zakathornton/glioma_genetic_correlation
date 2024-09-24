726ec16d4fb193b3cbed54ee338986321ff6fd50
Zak Thornton <86413667+zakathornton@users.noreply.github.com>
2024-09-24 12:57:14 +0100

    Create FUMA_SNPs.R

---
+++ FUMA/FUMA_SNPs.R
@@ -0,0 +1,26 @@
+## load the SNP output from FUMA (for a given exposure or outcome)
+
+dat<-read.delim("~/OneDrive - University of Bristol/GenCorr/Data/Outcome/FUMA/snps.txt")
+
+## Set the locus parameters
+
+CHR<-17
+start<-69245591
+end<-70495119
+
+## Subset the data to the locus parameters
+
+dat<-dat[dat$chr==CHR,]
+dat<-dat[dat$pos>start&dat$pos<end,]
+
+# Filter and re-order the data
+
+```{r}
+dat<-dat[c(3,4,2,6,5,7,9,10,8,12,11,14,15,16)]
+```
+
+## Save the table
+
+```{r}
+write.table(dat, "~/OneDrive - University of Bristol/GenCorr/Data/Outcome/all_glioma_FUMA_SNPs_157.txt", quote = F, row.names = F)
+```
