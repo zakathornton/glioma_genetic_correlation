9e40945e59f2272de8b0645bbb23d6e4741a46da
Zak Thornton <86413667+zakathornton@users.noreply.github.com>
2024-09-24 15:31:37 +0100

    Update heritability.sh

--- LDSC/heritability.sh
+++ LDSC/heritability.sh
@@ -1,6 +1,6 @@
-module load languages/python/ldsc-2.0.1
-ldsc.py \
---h2 oropharyngeal_rashkin.sumstats.gz \
---ref-ld-chr eur_w_ld_chr/ \
---w-ld-chr eur_w_ld_chr/ \
---out oropharyngeal_rashkin_h2
+module load languages/python/ldsc-2.0.1            #load modules
+ldsc.py \                                          #run the python script
+--h2 oropharyngeal_rashkin.sumstats.gz \           #provide the summary stats for heritability analysis
+--ref-ld-chr eur_w_ld_chr/ \                       #
+--w-ld-chr eur_w_ld_chr/ \                         #
+--out oropharyngeal_rashkin_h2                     #Specify the outcome name
