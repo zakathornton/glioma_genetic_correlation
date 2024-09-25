c77c28d95e7f76361a7475c2611fb57b0884b763
Zak Thornton <86413667+zakathornton@users.noreply.github.com>
2024-09-25 09:59:24 +0100

    Create lung_cleaning.R

---
+++ Data_cleaning/lung_cleaning.R
@@ -0,0 +1,46 @@
+## Load modules
+
+library(data.table)
+
+## Load the raw data
+
+data<-read.csv("lung_raw.tsv", sep = "\t")
+
+## Extract the variables needed (CHR, POS, SNP, A1, A2, FREQA1, BETA, SE, P, N)
+
+data<-data[c(3,4,1,10,11,12,8,9,2)]
+
+## Appropriately rename columns
+
+colnames(data)[1]<-"CHR"
+colnames(data)[2]<-"BP"
+colnames(data)[3]<-"SNP"
+colnames(data)[4]<-"A1"
+colnames(data)[5]<-"A2"
+colnames(data)[6]<-"FREQA1"
+colnames(data)[7]<-"BETA"
+colnames(data)[8]<-"SE"
+colnames(data)[9]<-"P"
+
+## Ensure all numeric values are numeric
+
+data$FREQA1<-as.numeric(data$FREQA1)
+data$BETA<-as.numeric(data$BETA)
+data$SE<-as.numeric(data$SE)
+data$P<-as.numeric(data$P)
+
+## Remove any SNPs with MAF<0.01
+
+data<-data[data$FREQA1>0.01&data$FREQA1<0.99,]
+
+## Only keep autosomal chromosomes
+
+data<-data[data$CHR%between%c(1,22),]
+
+## Keep unique SNPs only (remove duplicates)
+
+data<-data[!duplicated(data$SNP),]
+
+## Save the new data
+
+write.table(data, "lung_data.txt", row.names = F, quote = F)
